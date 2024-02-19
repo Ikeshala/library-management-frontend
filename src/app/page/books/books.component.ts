9e7ea063c9190f331930e66c6c5f757735b32abc
Ikeshala <ikeshala.peiris@gmail.com>
2024-02-19 16:47:43 +0530

    Successfully loaded all books and displayed them on the page.

--- src/app/page/books/books.component.ts
+++ src/app/page/books/books.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule, CommonModule],
   templateUrl: './books.component.html',
-  styleUrl: './books.component.css'
+  styleUrl: './books.component.css',
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit {
+  private http;
+  public bookList: any = {};
 
+  constructor(private httpCliant: HttpClient) {
+    this.http = httpCliant;
+  }
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    this.http.get('http://localhost:8080/book/get').subscribe((data) => {
+      this.bookList = data;
+      console.log(this.bookList);
+    });
+  }
 }
