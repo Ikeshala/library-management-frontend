188d2e79c20464e16069c9f24ef150394a069c01
Ikeshala <ikeshala.peiris@gmail.com>
2024-02-19 17:09:46 +0530

    Successfully loaded all borrowers and displayed their details.

--- src/app/page/borrowers/borrowers.component.ts
+++ src/app/page/borrowers/borrowers.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-borrowers',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule, CommonModule],
   templateUrl: './borrowers.component.html',
-  styleUrl: './borrowers.component.css'
+  styleUrl: './borrowers.component.css',
 })
-export class BorrowersComponent {
+export class BorrowersComponent implements OnInit {
+  private http;
+  public borrowerList: any = {};
 
+  constructor(private httpCliant: HttpClient) {
+    this.http = httpCliant;
+  }
+
+  ngOnInit(): void {
+    this.loadBorrowers();
+  }
+
+  loadBorrowers() {
+    this.http.get('http://localhost:8080/borrower/get').subscribe((data) => {
+      this.borrowerList = data;
+      console.log(this.borrowerList);
+    });
+  }
 }
