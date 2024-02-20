85967c28449c14a809b9c3d2dbc3db5f74c9804e
Ikeshala <ikeshala.peiris@gmail.com>
2024-02-20 16:39:43 +0530

    Configured routes in the Book component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BooksComponent } from './page/books/books.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'books',
+    component: BooksComponent,
+  },
+];
