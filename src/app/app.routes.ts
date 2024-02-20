3793e19701b26d162dc27ec4d920f1ef48998c1b
Ikeshala <ikeshala.peiris@gmail.com>
2024-02-20 18:15:27 +0530

    Created the home component and configured the routes accordingly.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { BooksComponent } from './page/books/books.component';
 import { BorrowersComponent } from './page/borrowers/borrowers.component';
 import { LoginComponent } from './common/login/login.component';
 import { SignupComponent } from './common/signup/signup.component';
+import { HomeComponent } from './page/home/home.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,10 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
   },
   {
     path: 'books',
