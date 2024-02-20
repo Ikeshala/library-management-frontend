17c1dc08b05955b417744d99cef67ae88b2cc3d7
Ikeshala <ikeshala.peiris@gmail.com>
2024-02-20 17:58:14 +0530

    Implemented the signup interface and integrated an API to load all countries along with their relevant country codes.

--- src/app/common/signup/signup.component.ts
+++ src/app/common/signup/signup.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule, CommonModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  private http;
+  public countryList: any;
+  public selectedCountry: any;
 
+  constructor(private httpCliant: HttpClient) {
+    this.http = httpCliant;
+  }
+
+  ngOnInit(): void {
+    this.loadCountries();
+  }
+
+  loadCountries() {
+    let api = 'https://restcountries.com/v3.1/all';
+    this.http.get(api).subscribe((res) => {
+      this.countryList = res;
+      console.log(res);
+    });
+  }
+
+  setSelectedCountry(country: any) {
+    this.selectedCountry = country;
+  }
 }
