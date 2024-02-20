557606fc5ccf67106c32b8b5998db6ebce1044e5
Ikeshala <ikeshala.peiris@gmail.com>
2024-02-20 16:47:58 +0530

    Created the login component and configured routes for it.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { BooksComponent } from './page/books/books.component';
 import { BorrowersComponent } from './page/borrowers/borrowers.component';
+import { LoginComponent } from './common/login/login.component';
 
 export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
   {
     path: 'books',
     component: BooksComponent,
