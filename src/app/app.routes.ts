2bc8880abfa358c829c7ba3ff28e8741d71a96ed
Ikeshala <ikeshala.peiris@gmail.com>
2024-02-20 17:14:55 +0530

    Created the signup component and configured routes for it.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { BooksComponent } from './page/books/books.component';
 import { BorrowersComponent } from './page/borrowers/borrowers.component';
 import { LoginComponent } from './common/login/login.component';
+import { SignupComponent } from './common/signup/signup.component';
 
 export const routes: Routes = [
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
   {
     path: 'login',
     component: LoginComponent,
