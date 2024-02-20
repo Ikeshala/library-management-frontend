69781a3cd8a787cf51e150cee7e3a38af7e70eb9
Ikeshala <ikeshala.peiris@gmail.com>
2024-02-20 16:42:16 +0530

    Configured routes for the Borrower component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { BooksComponent } from './page/books/books.component';
+import { BorrowersComponent } from './page/borrowers/borrowers.component';
 
 export const routes: Routes = [
   {
     path: 'books',
     component: BooksComponent,
+  },
+  {
+    path: 'borrowers',
+    component: BorrowersComponent,
   },
 ];
